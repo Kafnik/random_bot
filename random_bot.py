b86fd4c718f573658390d0d84a3611d25008fcaa
kafnik <kafnik0009000@gamil.com>
2025-10-14 20:59:36 +0500

    update 1.3

--- random_bot.py
+++ random_bot.py
@@ -6,134 +6,152 @@ from time import sleep
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
-VERSION = 1.2
+VERSION = 1.3
 x = 1000
-junk_food= ["Пицца", "Бургер", "Суши","Салат","Рамен","Картошка фри","Шаурма"]
-orel_talis = ["Орел","Решка"]
+foods = ["🧠Варенный мозг", "🎃Суп из тьмы", "💀Паучий пирог", "🩸Кровавый смузи", "🎃Пирожок с тыквой"]
+coin = ["👁Орел потусторонний", "🩸Решка демона"]
+roles = [ "🧙 Ведьма из тумана",
+            "🧛 Вампир с красными глазами",
+            "🧟‍♂️ Зомби, ищущий мозги",
+            "🕸 Призрак старого замка",
+            "🐺 Оборотень из ночного леса",
+            "🎭 Таинственный незнакомец"]
+treats = ["🍫 Шоколадка с привидениями",
+            "🍭 Карамельный череп",
+            "🍪 Печенье с пауками",
+            "🍬 Конфета с сюрпризом",
+            "🧃 Кровавый сок (клюквенный конечно!)"]
+mosters = [ "👹 Крик болотного чудовища",
+            "🦇 Вампир-охотник за сладостями",
+            "🪦 Призрак кладбища",
+            "💀 Скелет, танцующий в тумане",
+            "🕷 Огромный паук с красными глазами"]
+challenges = [
+            "🎃 Расскажи страшную историю!",
+            "💀 Сделай фото в темноте и отправь другу",
+            "🧟‍♀️ Притворись зомби на 10 секунд",
+            "🕸️ Скажи 'Бу!' кому-нибудь неожиданно",
+            "🦇 Придумай себе страшное имя"
+        ]
+spooky_phrases = ["🕯 Призываю духов...",
+        "🌑 Тьма сгущается...",
+        "🕸 Что-то движется в темноте...",
+        "💀 Шепот из темноты...",
+        "🦇 Ночь пробуждает монстров..."
+    ]
 
 
+def complate_play(message):
+    msg = bot.send_message(message.chat.id, random.choice(spooky_phrases))
+    sleep(1)
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    btn1 = types.InlineKeyboardButton("🎲Рандомное число", callback_data='btn1')
+    btn2 = types.InlineKeyboardButton("🍔Рандомная еда", callback_data='btn2')
+    btn3 = types.InlineKeyboardButton("🪙Орел или решка", callback_data='btn3')
+    who = types.InlineKeyboardButton("🧛‍♂️Кто ты на Хуллоуин?", callback_data="who")
+    treat = types.InlineKeyboardButton("🍬Угощение или жизнь", callback_data="treat")
+    monster = types.InlineKeyboardButton("👻Монстр дня",callback_data="monster")
+    challenge = types.InlineKeyboardButton("🎲Испытание",callback_data="challenge")
+    markup.add(btn1, btn2, btn3, who, treat, monster, challenge)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text="🎃 *Игра завершена!* 🎃\n\n"
+            "Вы отлично справились, смертный!\n\n"
+            "👇 *Выбери следующую игру или рискни вызвать духов снова...* 👇",parse_mode='Markdown',
+        reply_markup=markup)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'OpenbotAI')
+    msg = bot.send_message(message.chat.id, 'OpenbotAI')
     sleep(1)
-    bot.send_message(message.chat.id, 'Привет я бот рандома пропиши команду /random. Или ознакомьтесь со списком команд напишите в чат /help')
+    bot.edit_message_text(f'Привет {message.from_user.first_name} \nЯ бот рандома пропиши команду /random чтобы поиграть в игры.', message_id=msg.message_id, chat_id=message.chat.id)
 
-@bot.message_handler(commands=['random'])
-def random_mune(message):
-    markup = types.InlineKeyboardMarkup(row_width=1)
-    food = types.InlineKeyboardButton('Случайная еда', callback_data='food1')
-    orel = types.InlineKeyboardButton('Орел решка', callback_data='orel1')
-    number = types.InlineKeyboardButton('Случайное число', callback_data='number1')
-    markup.add(food, orel, number)
-    bot.send_message(message.chat.id, f'Добро пожаловать в меню игр {message.from_user.first_name}', reply_markup=markup)
+#-----------------Обновление 1.3----------------------
+@bot.message_handler(commands=['halloween'])
+def halloween(message):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    who = types.InlineKeyboardButton("🧛🏻‍♀️Кто ты на Хуллоуин?", callback_data="who")
+    treat = types.InlineKeyboardButton("🍬Угощение или жизнь", calk_data="treat")
+    monster = types.InlineKeyboardButton("👻Монстр дня",callback_data="monster")
+    challenge = types.InlineKeyboardButton("🎲Испытание",callback_data="challenge")
+    markup.add(who, treat, monster, challenge)
+    bot.send_message(message.chat.id, "🎃 Добро пожаловать в *Halloween Random!*", reply_markup=markup)
     
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    if call.data =='food1':
-        markup = types.InlineKeyboardMarkup(row_width=1)
-        random_food = types.InlineKeyboardButton('Сделать случайную еду', callback_data='eda')
-        markup.add(random_food)
-        bot.send_message(call.message.chat.id, 'Напишите в чат /random_food или нажми кнопку', reply_markup=markup)
-        
-    elif call.data == 'eda':
-        result = random.choice(junk_food)
-        bot.send_message(call.message.chat.id, f'Сегодня тебе выподает: {result}')
-        sleep(1)
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        yes = types.InlineKeyboardButton('Да', callback_data='yes1')
-        no = types.InlineKeyboardButton('Нет', callback_data='no1')
-        markup.add(yes, no)
-        bot.send_message(call.message.chat.id, 'Желаете продолжить игру ?', reply_markup=markup)
 
-    elif call.data == 'orel1':
-        markup = types.InlineKeyboardMarkup(row_width=1)
-        orel = types.InlineKeyboardButton('Орел или решка ?🤔', callback_data='num2')
-        markup.add(orel)
-        bot.send_message(call.message.chat.id, 'Напишите в чат /random_orel или нажми кнопку', reply_markup=markup)
+# ---------------- Меню рандомных игр ----------------
+@bot.message_handler(commands=['random'])
+def random_menu(message):
+    msg = bot.send_message(message.chat.id, 'Загрузка...')
+    sleep(1)
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    btn1 = types.InlineKeyboardButton("🎲Рандомное число", callback_data='btn1')
+    btn2 = types.InlineKeyboardButton("🍔Рандомная еда", callback_data='btn2')
+    btn3 = types.InlineKeyboardButton("🪙Орел или решка", callback_data='btn3')
+    markup.add(btn1, btn2, btn3)
+    bot.edit_message_text('Меню игр открыто!', reply_markup=markup, message_id=msg.message_id, chat_id=message.chat.id)
+
+@bot.callback_query_handler(func=lambda m: True)
+def  callback_data(call):
+    if call.data == 'btn1':
+        msg = bot.send_message(call.message.chat.id, 'Генерирую...')
+        sleep(1)
+        number = random.randint(1, 666)
+        bot.edit_message_text(f"Твое число: {number}", message_id=msg.message_id, chat_id=call.message.chat.id)
+        sleep(1)
+        bot.answer_callback_query(call.id)
+        complate_play(call.message)
+
+    elif call.data == 'btn2': 
+        food = random.choice(foods)
+        bot.send_message(call.message.chat.id, f'Тебе выподает: {food}')
+        sleep(1)
+        bot.answer_callback_query(call.id)
+        complate_play(call.message)
+
+
+    elif call.data == 'btn3':
+        orel = random.choice(coin)
+        bot.send_message(call.message.chat.id, f'Тебе выподает: {orel}')
+        sleep(1)
+        bot.answer_callback_query(call.id)
+        complate_play(call.message)
+
+
+#-------------Хелуин------------
+    elif call.data == 'who':
+        relust = random.choice(roles)
+        bot.send_message(call.message.chat.id, f'На Хэллоуин ты - {relust}')
+        sleep(1)
+        bot.answer_callback_query(call.id)
+        complate_play(call.message)
+
+    elif call.data == 'treat':
+        relust = random.choice(treats)
+        bot.send_message(call.message.chat.id, f'Тебе досталось: {relust}')
+        sleep(1)
+        bot.answer_callback_query(call.id)
+        complate_play(call.message)
+
+    elif call.data == 'monster':
+        relust = random.choice(mosters)
+        bot.send_message(call.message.chat.id, f'Сегодня тебя преследует: {relust}')
+        sleep(1)
+        bot.answer_callback_query(call.id)
+        complate_play(call.message)
   
-    elif call.data == 'num2':
-        num = random.choice(orel_talis)
-        bot.send_message(call.message.chat.id, f'Тебе вы подает: {num}')
+    elif call.data == 'challenge':
+        relust = random.choice(challenges)
+        bot.send_message(call.message.chat.id, f'Твое испытание: {relust}')
         sleep(1)
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        yes = types.InlineKeyboardButton('Да', callback_data='yes2')
-        no = types.InlineKeyboardButton('Нет', callback_data='no1')
-        markup.add(yes, no)
-        bot.send_message(call.message.chat.id, 'Желаете продолжить игру ?', reply_markup=markup)
-        
-    elif call.data == 'yes1':
-        result = random.choice(junk_food)
-        bot.send_message(call.message.chat.id, f'Сегодня тебе выподает: {result}')
-        sleep(1)
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        yes = types.InlineKeyboardButton('Да', callback_data='yes1')
-        no = types.InlineKeyboardButton('Нет', callback_data='no1')
-        markup.add(yes, no)
-        bot.send_message(call.message.chat.id, 'Желаете продолжить игру ?', reply_markup=markup) 
-
-    elif call.data == 'yes2':
-        num = random.choice(orel_talis)
-        bot.send_message(call.message.chat.id, f'Тебе вы подает: {num}')
-        sleep(1)
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        yes = types.InlineKeyboardButton('Да', callback_data='yes2')
-        no = types.InlineKeyboardButton('Нет', callback_data='no1')
-        markup.add(yes, no)
-        bot.send_message(call.message.chat.id, 'Желаете продолжить игру ?', reply_markup=markup)
-
-    elif call.data == 'number1':
-        markup = types.InlineKeyboardMarkup(row_width=1)
-        number2 = types.InlineKeyboardButton('Случайное число', callback_data='number3')
-        markup.add(number2)
-        bot.send_message(call.message.chat.id, 'Напишите в чат /random_number или нажми кнопку', reply_markup=markup)
-
-    elif call.data == 'number3':
-        number = random.randint(1, x)
-        bot.send_message(call.message.chat.id, f'Ваше случайное число: {number}')
-        sleep(1)
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        yes = types.InlineKeyboardButton('Да', callback_data='yes3')
-        no = types.InlineKeyboardButton('Нет', callback_data='no1')
-        markup.add(yes, no)
-        bot.send_message(call.message.chat.id, 'Желаете продолжить игру ?', reply_markup=markup)
-
-    elif call.data == 'yes3':
-        number1 = random.randint(1, x)
-        bot.send_message(call.message.chat.id, f'Ваше случайное число: {number1}')
-        sleep(1)
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        yes = types.InlineKeyboardButton('Да', callback_data='yes3')
-        no = types.InlineKeyboardButton('Нет', callback_data='no1')
-        markup.add(yes, no)
-        bot.send_message(call.message.chat.id, 'Желаете продолжить игру ?', reply_markup=markup)
-
-    elif call.data == 'no1':
-         bot.answer_callback_query(call.id)
-         random_mune(call.message)
-
-@bot.message_handler(commands=['random_orel'])
-def random_orel_user(message):
-    num = random.choice(orel_talis)
-    bot.send_message(message.chat.id, f'Тебе вы подает: {num}')
-
-@bot.message_handler(commands=['random_food'])
-def random_food_user(message):
-    result = random.choice(junk_food)
-    bot.send_message(message.chat.id, f'Сегодня тебе выподает: {result}')
-
-@bot.message_handler(commands=['random_number'])
-def random_bot(message):
-    number = random.randint(1, x)
-    bot.send_message(message.chat.id, f'Ваше случайное число: {number}')
+        bot.answer_callback_query(call.id)
+        complate_play(call.message)
 
 @bot.message_handler(commands=['help'])
 def info(message):
-    bot.send_message(message.chat.id, 'В данном боте присутствуют команды ')
+    bot.send_message(message.chat.id, 'В данном боте присутствуют команды /start, /info, /help, /random, /random, /random_orel, random_food, /random_number')
     
 @bot.message_handler(commands=['info'])
 def info_user(message):
     bot.send_message(message.chat.id, f'Бот cделан команиями: OpenbotAI и VECTORBOT \nНо большую чать выполнила комания: OpenbotAI \nЧто есть прикольного команда /credit \nВерсия бота: {VERSION}')
 
+# ---------------- Запуск бота ----------------
 print('Рандом бот запушен!')
-bot.polling(non_stop=True)
+bot.polling(none_stop=True)
